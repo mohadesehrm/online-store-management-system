7e8b63961e5587e07826b2005491922ef34185a1
Mohadeseh <mohadesehmoghadam2010@gmail.com>
2026-08-19 15:13:15 +0330

    feat: add best-selling products report

---
+++ reports.py
@@ -0,0 +1,16 @@
+from utils import load
+orders = load("data/orders.json")
+# Display the best-selling products.
+def best_selling_products():
+    sales = {}
+    for order in orders:
+        for product in order["products"]:
+            product_id = product["product_id"]
+            quantity = product["quantity"]
+            if product_id in sales:
+                sales[product_id] += quantity
+            else:
+                sales[product_id] = quantity
+    sorted_sales = sorted(sales.items(),key=lambda i: i[1],reverse=True)
+    for product_id, quantity in sorted_sales:
+        print(f"Product ID: {product_id}\nSold: {quantity}")
